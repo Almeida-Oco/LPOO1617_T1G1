6165ed93d7d140f407e48044e187f2bd1955c872
TrocaTudo95 <pedromachado_95@sapo.pt>
2017-02-23 15:26:32 +0000

    userInput and game logic started

--- tp2/dkeep/logic/GameLogic.java
+++ tp2/dkeep/logic/GameLogic.java
@@ -1,5 +1,34 @@
 package dkeep.logic;
 
 public class GameLogic{
+	private Map map;
+	private Ogre ogres[];
+	private Hero hero ;
+	private Guard guard;
+	private int[] key;
+	
+	public Game(Map game_map){
+		map =game_map;
+		hero=new Hero();
+		guard=new Guard();
+		
+	}
+	
+	public boolean isGameOver( ){
+	
+		for(int i=0; i<ogres.size();i++){
+			if(ogres[i].checkGameOver())
+				return true;
+		}
+		if(guard.checkGameOVer())
+		return true;
+		
+		else
+			return false;
+	}
+	
+
+	
+	
 	
 }

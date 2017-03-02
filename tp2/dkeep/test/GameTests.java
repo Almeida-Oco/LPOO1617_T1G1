ea7c18c305f3f16d5d3dba135ae6d4c16762602e
Almeida-Oco <intjalmeida@gmail.com>
2017-03-02 13:53:34 +0000

    added tests

--- tp2/dkeep/test/GameTests.java
+++ tp2/dkeep/test/GameTests.java
@@ -1,11 +1,16 @@
 package dkeep.test;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import dkeep.logic.Map;
 
 public class GameTests {
-
+	char[][] map = {{'X','X','X','X','X'},
+					{'X','H',' ','G','X'},
+					{'I',' ',' ',' ','X'},
+					{'I','k',' ',' ','X'},
+					{'X','X','X','X','X'}};
+	}
 	@Test
 	public void test() {
 		fail("Not yet implemented");

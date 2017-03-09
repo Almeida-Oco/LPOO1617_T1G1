e6672b754b6008f844b4174edf920590da847bcc
Almeida-Oco <intjalmeida@gmail.com>
2017-03-09 14:47:00 +0000

    no major changes

--- tp2/dkeep/gui/GameWindow.java
+++ tp2/dkeep/gui/GameWindow.java
@@ -41,7 +41,7 @@ public class GameWindow {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
+		//frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
